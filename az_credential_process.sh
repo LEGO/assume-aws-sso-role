1c13b8dfd0758678676e70f11565d73b4b064b55
Jonas Holst Damtoft <jonas.holst.damtoft@lego.com>
2023-06-26 17:11:34 +0200

    initial attempt

---
+++ az_credential_process.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+awsaccount=$1
+awsrole=$2
+
+if [ -z $awsaccount ] || [ -z $awsrole ]; then
+	echo "Usage $0 <account> <role>"
+	exit 1
+fi
+
+tenant="1d063515-6cad-4195-9486-ea65df456faa"
+saml2oidc="3cd4d944-d89b-401b-b2ae-fb1ece182362"
+
+awssso="https://signin.aws.amazon.com/saml/$awsaccount"
+role="arn:aws:iam::$awsaccount:role/$awsrole"
+provider="arn:aws:iam::$awsaccount:saml-provider/AzureAD"
+
+tokenExchangeUri="https://ws-iam-commontools-oidc2saml.azurewebsites.net/api/TokenExchange/SAMLResponse?IdentifierUri=$awssso"
+
+az account show >/dev/null 2>&1 || az login --tenant $tenant >/dev/null
+result=$?
+if [ $result -ne 0 ]; then
+	echo >&2 "Error logging in to AzureAD"
+	exit result
+fi
+
+result=$?
+token=$(az account get-access-token --resource api://$saml2oidc --query accessToken --output tsv)
+if [ $result -ne 0 ]; then
+	echo >&2 "Error fetching token from AzureAD"
+	exit result
+fi
+
+saml=$(curl --no-progress-meter -H "Authorization: Bearer $token" $tokenExchangeUri)
+result=$?
+if [ $result -ne 0 ]; then
+	echo >&2 "Error exchanging token"
+	exit result
+fi
+
+aws sts assume-role-with-saml --role-arn $role --principal-arn $provider --saml-assertion $saml | jq ".Credentials" | jq ".Version = 1"
