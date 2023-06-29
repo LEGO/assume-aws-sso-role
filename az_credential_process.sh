07a9b8ba9cbe0672e968781ee160c04eb89d92a2
Jonas Holst Damtoft <jonas.holst.damtoft@lego.com>
2023-06-29 10:39:21 +0200

    add some extra output for progress

--- az_credential_process.sh
+++ az_credential_process.sh
@@ -3,7 +3,7 @@
 saml2oidc="3cd4d944-d89b-401b-b2ae-fb1ece182362"
 tenant="1d063515-6cad-4195-9486-ea65df456faa"
 
-credentials_path=${XDG_CACHE_HOME:-"$HOME/cache"}/aws_az_credentials/
+credentials_path="${XDG_CACHE_HOME:-"$HOME/.cache"}/aws_az_credentials/"
 mkdir -p $credentials_path
 
 awsaccount="$1"
@@ -27,6 +27,8 @@ if [ ! -z "$creds" ]; then
 	fi
 fi
 
+echo >&2 "No credentials found, fetching new credentials"
+
 awssso="https://signin.aws.amazon.com/saml/$awsaccount"
 role="arn:aws:iam::$awsaccount:role/$awsrole"
 provider="arn:aws:iam::$awsaccount:saml-provider/AzureAD"
@@ -40,6 +42,7 @@ if [ $result -ne 0 ]; then
 	exit result
 fi
 
+echo >&2 "Getting AzureAD token"
 token=$(az account get-access-token --resource api://$saml2oidc --query accessToken --output tsv)
 result=$?
 if [ $result -ne 0 ]; then
@@ -47,6 +50,7 @@ if [ $result -ne 0 ]; then
 	exit result
 fi
 
+echo >&2 "Exchanging token for saml response"
 saml=$(curl --no-progress-meter -H "Authorization: Bearer $token" $tokenExchangeUri)
 result=$?
 if [ $result -ne 0 ]; then
@@ -54,6 +58,8 @@ if [ $result -ne 0 ]; then
 	exit result
 fi
 
+echo >&2 "Assuming AWS role $awsrole for $awsaccount"
 creds=$(env -u AWS_PROFILE aws sts assume-role-with-saml --no-sign-request --role-arn $role --principal-arn $provider --saml-assertion $saml --query "Credentials" | jq ".Version = 1")
 echo $creds >$credentials_file
+echo >&2 "Credentials expire $(jq -r ".Expiration" $credentials_file)"
 echo $creds
