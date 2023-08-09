83992cd0d8aa8d3b551ae8dd3a27a66fdd17b4f5
Jonas Holst Damtoft <jonas.holst.damtoft@lego.com>
2023-08-09 17:51:22 +0200

    fix formula

--- assume-aws-sso-role.rb
+++ assume-aws-sso-role.rb
@@ -3,7 +3,7 @@ class AssumeAwsSsoRole < Formula
   homepage "https://github.com/LEGO/assume-aws-sso-role"
   version "0.5.0"
 
-  url "https://raw.githubusercontent.com/LEGO/assume-aws-sso-role/v0.5.0/assume-aws-sso-role",
+  url "https://raw.githubusercontent.com/LEGO/assume-aws-sso-role/v0.5.0/assume-aws-sso-role"
   sha256 "82f7405552bda26ae16fa9dcfc448257d28acad39c0d345ebdb9d49770590da0"
 
   def install
