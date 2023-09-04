cbb203a43467ec7c0fecf665d543560feb10ef98
Jonas Holst Damtoft <56115984+jhd-lego@users.noreply.github.com>
2023-09-04 17:11:17 +0200

    add ci/cd (#11)
    * add ci/cd
    
    * make shellcheck happy

---
+++ .github/scripts/update-formula.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# https://github.com/LEGO/amma-cli/blob/46e3fd94e7bf57820376a08b2edb21abdd636ffb/.github/scripts/update-formula.sh
+set -euxo pipefail
+temp_dir="$(mktemp -d)"
+trap "{ rm -rf ""$temp_dir""; }" EXIT
+
+pushd "$temp_dir"
+gh release download --repo LEGO/assume-aws-sso-role --pattern 'assume-aws-sso-role'
+checksum="$(sha256sum assume-aws-sso-role | awk '{ print $1 }')"
+popd
+
+latest_version=$(gh release view --json name --jq '.name[1:]')
+latest_release="$(gh api repos/LEGO/assume-aws-sso-role/releases/latest --jq '[.assets[] | select(.name | startswith("assume-aws-sso-role"))] | map({(.name): .url}) | add')"
+url="$(jq --raw-output '."assume-aws-sso-role"' <<<"$latest_release")"
+
+sed -i.old \
+  -e "s|version \".*\"|version \"$latest_version\"|" \
+  -e "s|url \".*\"|url \"$url\"|" \
+  -e "s|sha256 \".*\"|sha256 \"$checksum\"|" \
+  assume-aws-sso-role.rb
+rm assume-aws-sso-role.rb.old
