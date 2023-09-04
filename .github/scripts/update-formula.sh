4ef0b67be8486e0fff96f94473b42c3237d2d723
Jonas Holst Damtoft <jonas.holst.damtoft@lego.com>
2023-09-04 14:22:08 +0200

    make shellcheck happy

--- .github/scripts/update-formula.sh
+++ .github/scripts/update-formula.sh
@@ -2,7 +2,7 @@
 # https://github.com/LEGO/amma-cli/blob/46e3fd94e7bf57820376a08b2edb21abdd636ffb/.github/scripts/update-formula.sh
 set -euxo pipefail
 temp_dir="$(mktemp -d)"
-trap "{ rm -rf "$temp_dir"; }" EXIT
+trap "{ rm -rf ""$temp_dir""; }" EXIT
 
 pushd "$temp_dir"
 gh release download --repo LEGO/assume-aws-sso-role --pattern 'assume-aws-sso-role'
@@ -11,7 +11,7 @@ popd
 
 latest_version=$(gh release view --json name --jq '.name[1:]')
 latest_release="$(gh api repos/LEGO/assume-aws-sso-role/releases/latest --jq '[.assets[] | select(.name | startswith("assume-aws-sso-role"))] | map({(.name): .url}) | add')"
-url="$(jq --raw-output '."assume-aws-sso-role"' <<<$latest_release)"
+url="$(jq --raw-output '."assume-aws-sso-role"' <<<"$latest_release")"
 
 sed -i.old \
   -e "s|version \".*\"|version \"$latest_version\"|" \
