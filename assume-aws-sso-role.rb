50467538d5b8c758568922ff0a8c630e78299348
assume-aws-sso-role <assume-aws-sso-role@LEGO.com>
2023-10-03 08:55:12 +0000

    Update homebrew formula

--- assume-aws-sso-role.rb
+++ assume-aws-sso-role.rb
@@ -1,13 +1,13 @@
 class AssumeAwsSsoRole < Formula
   desc "A CLI tool for assuming AWS roles via SSO"
   homepage "https://github.com/LEGO/assume-aws-sso-role"
-  version "0.8.0"
+  version "0.9.0"
 
-  url "https://api.github.com/repos/LEGO/assume-aws-sso-role/releases/assets/124510094",
+  url "https://api.github.com/repos/LEGO/assume-aws-sso-role/releases/assets/128808582",
       headers: [
         "Accept: application/octet-stream",
       ]
-  sha256 "3ca9c449a93acae4750926eb19f76350077023e4b2a5b54ae2379b742e76a2e3"
+  sha256 "fc8247e31f6dd01a8949a231e876e7b0e16ee947e3515737755961fe24d016fd"
 
   def install
     bin.install "assume-aws-sso-role"
